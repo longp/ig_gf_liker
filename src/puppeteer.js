326f32b1bac43def2fcf0af1b7e3cde24ea530d8
long <long@longs-MBP.fios-router.home>
2018-11-11 22:19:52 -0500

    got to log in to my ig

--- src/puppeteer.js
+++ src/puppeteer.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const puppeteer = require('puppeteer');
 let config = require('../config/config.json');
 
@@ -5,15 +7,30 @@ let run = async function () {
 
     // set up Puppeteer
     const browser = await puppeteer.launch({
-        headless: conf.settings.headless,
+        // headless: config.settings.headless,
+        headless: config.settings.headless,
         args: ['--no-sandbox']
     });
 
     const page = await browser.newPage();
+    // Load Instagram
+    await page.goto('https://www.instagram.com');
+    await page.waitFor(2500);
+    await page.click(config.selectors.home_to_login_button);
+    await page.waitFor(2500);
+
+    // Login
+    await page.click(config.selectors.username_field);
+    await page.keyboard.type(process.env.IG_USER);
+    await page.click(config.selectors.password_field);
+    await page.keyboard.type(process.env.IG_PASS);
+
+    await page.click(config.selectors.login_button);
+    await page.waitForNavigation();
     page.setViewport({width: 1200, height: 764});
     
     // Close browser
-    browser.close();
+    // browser.close();
 };
 
 module.exports = run;
